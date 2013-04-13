c61485604195d53da2959c6d0b5cff68584f98dd
Walmir Neto <wfsneto@gmail.com>
2013-04-13 18:41:23 -0300

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetYourBook::Application.config.secret_token = '794f28cb6976d3a904b9ce4ad37434abf3551a696d5c9c007be5b987547c80920dea29c3fc5fa20b4f581a26f750b1832cb2eb3d9f6b1239a63798cdf1729bdf'
