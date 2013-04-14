ec9d284105938b4895d562d4fa1b0030324fe755
Walmir Neto <wfsneto@gmail.com>
2013-04-14 15:33:30 -0300

    implement search and filters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @authors = Author.all
+    
     @books = Book.all
+    @books = @books.by_title params[:title] if params[:title].present?
+    @books = @books.by_author_id params[:author_id] if params[:author_id].present?
+    @books = @books.available params[:is_available] if params[:is_available].present?
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,13 @@
 class Book < ActiveRecord::Base
+  # attribute accessible
   attr_accessible :author_id, :image, :is_available, :title
 
+  # relationships
   belongs_to :author
+
+  # scopes
+  scope :all, lambda { order("title DESC") }
+  scope :by_title, lambda { |title| where("title LIKE ?", "%#{title}%") }
+  scope :available, lambda { |is_available| where("is_available = ?", !is_available.to_i.zero?) }
+  scope :by_author_id, lambda { |author_id| where("author_id = ?", author_id) }
 end
