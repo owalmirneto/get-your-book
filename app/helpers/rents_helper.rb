c55a9ba74cab11e76333f66d18a2163df7498b66
Walmir Neto <wfsneto@gmail.com>
2013-04-16 23:02:05 -0300

    remove assets and listing rents

--- app/helpers/rents_helper.rb
+++ app/helpers/rents_helper.rb
@@ -1,2 +1,6 @@
 module RentsHelper
+  def statuses(rent)
+    type = rent.status != "delivered" ? "warning" : "success"
+    content_tag :span, rent.status, :class => "label label-#{type}"
+  end
 end
